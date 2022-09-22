c6b28055a416eaf00816a35df4e98374be3309a2
Christa Favano <crstav@gmail.com>
2022-09-22 00:01:12 -0400

    Add logic to check password

---
+++ script.js
@@ -0,0 +1,27 @@
+const checkPassword1 = document.querySelector('#password');
+const checkPassword2 = document.querySelector('#confirmPassword');
+
+// console.log(check)
+function checkPassword(){
+    const pass = document.querySelector('#password').value; 
+    const passConfirm = document.querySelector('#confirmPassword').value; 
+    if(pass != passConfirm) {
+        document.querySelector('.error-message').style.visibility = 'visible';
+    }
+    if(pass == passConfirm) {
+        document.querySelector('.error-message').style.visibility = 'hidden';
+    }
+}
+
+checkPassword1.addEventListener('mouseleave', checkPassword);
+checkPassword2.addEventListener('mouseleave', checkPassword);
+
+
+
+const formSignUp = document.querySelector('#signUpForm');
+
+formSignUp.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('Cancelled submission');
+
+});
